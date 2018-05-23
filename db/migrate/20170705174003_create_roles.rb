3a14f9f82179ff7fd56e35464e3af00dae4632f0
jasl <jasl9187@hotmail.com>
2018-05-23 19:36:46 +0800

    improve compatibility with mysql

--- db/migrate/20170705174003_create_roles.rb
+++ db/migrate/20170705174003_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[5.1]
   def change
     create_table :roles do |t|
       t.string :name, null: false
-      t.text :permissions, null: false
+      t.text :permissions
 
       t.string :type, null: false
 
